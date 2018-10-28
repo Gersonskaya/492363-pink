065c992a6881024e09022e64926b0c8899b02ee1
Дарья <immakovka@gmail.com>
2018-10-28 18:01:18 +0300

    Реализация меню через JS

---
+++ source/js/menu.js
@@ -0,0 +1,16 @@
+const button = document.getElementById("button");
+const body = document.getElementById("body");
+const menu = document.getElementById("menu");
+const header = document.getElementById("header");
+
+const buttonClass = 'main-nav__toggle--opened';
+const bodyClass = 'body--opened';
+const menuClass = 'main-nav__hidden--opened';
+const headerClass = 'page-header--opened';
+
+button.addEventListener("click", function() {
+  button.classList.toggle(buttonClass);
+  body.classList.toggle(bodyClass);
+  menu.classList.toggle(menuClass);
+  header.classList.toggle(headerClass);
+});
